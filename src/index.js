3504e01621715a433e3799411493187abecc0a53
VoThai <vtkghost@gmail.com>
2022-07-22 09:59:45 +0700

    moi

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <App />,
+const root = ReactDOM.createRoot(
   document.getElementById('root')
-  );
-
+);
+const element = <App />;
+root.render(element);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
